2f93837892f7fb1525566903a6cde88d390138a4
VladimirZausaev <zausaevladimir@gmail.com>
2023-04-03 16:39:39 +0300

    Last version of project

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-import numpy as np
 import requests
 from bokeh.plotting import figure, show
 from typing import List
-from datetime import datetime, date
 
 
 def get_temp(own_city_name: str) -> List:
-    api = "https://api.openweathermap.org/data/2.5/forecast?q=" + own_city_name + "&APPID=d438c29584f3e371f54cc81a6529daaf"
+    api = "https://api.openweathermap.org/data/2.5/forecast?q=" + \
+          own_city_name + "&APPID=d438c29584f3e371f54cc81a6529daaf"
 
     json_data = requests.get(api).json()
     temp = []
-    for i in json_data['list']:
-        temp.append(float(i['main']['temp'])-273)
+
+    for el in json_data['list']:
+        temp.append(float(el['main']['temp'])-273)
 
     return temp
 
 
 def get_date(own_city_name: str) -> List:
-    api = "https://api.openweathermap.org/data/2.5/forecast?q=" + own_city_name + "&APPID=d438c29584f3e371f54cc81a6529daaf"
+    api = "https://api.openweathermap.org/data/2.5/forecast?q=" + \
+          own_city_name + "&APPID=d438c29584f3e371f54cc81a6529daaf"
 
     json_data = requests.get(api).json()
 
     date = []
 
-    for i in json_data['list']:
-        data = i['dt_txt'].split(" ")
+    for j in json_data['list']:
+        data = j['dt_txt'].split(" ")
         time = data[1].split(":")
         data = data[0].split("-")
         date.append(int(data[2]) + int(time[0])/24)
@@ -35,24 +36,30 @@ def get_date(own_city_name: str) -> List:
 a = input('Введите город: ')
 
 
-y = list((get_temp(a)))
-x = list((get_date(a)))
+def painting_plot(own_city_name: str) -> None:
+    y = list((get_temp(own_city_name)))
+    x = list((get_date(own_city_name)))
 
-x_new = []
-y_new = []
+    x_new = []
+    y_new = []
 
-for i in range(0, len(y) - 1, 6):
-    y_new.append(y[i])
-    x_new.append(x[i])
+    for i in range(0, len(y) - 1, 7):
+        y_new.append(y[i])
+        x_new.append(x[i])
 
-plot = figure(
-    title=f'Погода: {a}',
-    x_axis_label='Дата',
-    y_axis_label='Температура'
-)
-plot.dot(x_new, y_new, size=45, color='red')
-plot.line(x, y, line_width=1.5)
-show(plot)
+    plot = figure(
+        title=f'Погода: {a}',
+        x_axis_label='Дата',
+        y_axis_label='Температура',
+    )
+
+    plot.dot(x_new, y_new, size=45, color='red')
+    plot.line(x, y, line_width=1.5)
+    show(plot)
 
 
+if __name__ == '__main__':
+    get_date(a)
+    get_temp(a)
+    painting_plot(a)
 
